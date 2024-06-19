5f12a0119e80ce885b5fccd2274205afa3aa4846
MIGUEL-ANGELHH <20230040@uthh.edu.mx>
2024-06-19 08:37:41 -0600

    Add files via upload

---
+++ Practica/index.php
@@ -0,0 +1,41 @@
+<?php
+
+    echo "My first php script"
+    echo <br>
+    echo "Mis datos:";
+    $nombre "Juan Rulfo";
+    $edad = 23;
+    $estatura = 1.56;
+    echo "soy....." . strtoupper($nombre) . "         tengo" . $edad . "años y mido" .$estatura . "metros";
+    echo <br>
+    $x = 34;
+    $y = 356.78;
+    $z = $x + $y;
+    echo "La suma es igual a $x + $Y = $z <br>";
+    $x = 60;
+    $y = 15;
+    $z = $x - $y;
+    echo "La resta es igual a $x + $Y = $z <br>";
+    $x = 26;
+    $y = 62;
+    $z = $x * $y;
+    echo "La multiplicacion es igual a $x + $Y = $z <br>";
+    $x = 100;
+    $y = 10;
+    $z = $x / $y;
+    echo "La division es igual a $x / $Y = $z <br>";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica PHP</title>
+</head>
+<body>
+    
+</body>
+</html>
